bbb6901016fcd2ecf92a35549a2eeeba36d5ca40
Leandro Gonçalves <leandrodebritogoncalves@gmail.com>
2021-02-25 18:10:29 -0400

    Sbstituição do HTML pelo js nas transações

--- scripts.js
+++ scripts.js
@@ -12,3 +12,69 @@ const Modal = {
         .classList.remove('active') 
     }
 }
+
+const transactions = [
+    {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date:'23/10/2021'
+    },
+
+    
+    {
+    id: 2,
+    description: 'Criação de WebSite',
+    amount: 500000,
+    date:'04/10/2021'
+    },
+
+
+    {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date:'20/10/2021'
+    },
+
+]
+
+const transaction = {
+    incomes () {
+        // Somar todas as entradas
+    },
+
+    expenses () {
+        // Somar as Saidas
+    },
+
+    total () {
+        // Entradas - Saidas
+    },
+}
+
+// Eu preciso pegar as minhas transações do
+// meu objeto aqui javascript
+// e colocar la no HTML
+
+const DOM = {
+    transactionContainer: document.querySelector('#data-table tbody'),
+    addtransactions(transactions, index) {
+        console.log(transactions)
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLtransactions(transactions)
+
+        DOM.transactionContainer.appendChild(tr)
+    },   
+    innerHTMLtransactions(transactions) {
+        const html = `
+             <td class="description">${transactions.description}</td>
+             <td class="expense">${transactions.amount}</td>
+             <td class="date">${transactions.date}</td>
+             <td><img src="./assets/minus.svg" alt="Icone Remover Itens"></td>
+        `
+        return html
+    },
+}
+
+DOM.addtransactions(transactions[0])
